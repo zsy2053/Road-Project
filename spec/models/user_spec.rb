d6a8f6ee4f0a586c5b93d7dd4bf574867d29e8de
Sunil Pai <spai@robinsonsolutions.com>
2017-12-21 20:34:39 +0000

    Fixed spacing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-	it { should belong_to(:site) }
+  it { should belong_to(:site) }
   
   it "has a valid factory" do
     expect(FactoryBot.build(:user)).to be_valid
   end
 
   describe "email attribute" do
-  	it "should not be nil" do
-  		expect(FactoryBot.build(:user, email: nil)).to_not be_valid
-  	end
+    it "should not be nil" do
+      expect(FactoryBot.build(:user, email: nil)).to_not be_valid
+    end
 
     it "cannot be blank" do
       expect(FactoryBot.build(:user, email: "")).to_not be_valid
@@ -18,9 +18,9 @@ RSpec.describe User, type: :model do
   end
 
   describe "username attribute" do
-  	it "should not be nil" do
-  		expect(FactoryBot.build(:user, username: nil)).to_not be_valid
-  	end
+    it "should not be nil" do
+      expect(FactoryBot.build(:user, username: nil)).to_not be_valid
+    end
 
     it "cannot be blank" do
       expect(FactoryBot.build(:user, username: "")).to_not be_valid
@@ -37,19 +37,19 @@ RSpec.describe User, type: :model do
     end
 
     it "must have at least one uppercase character" do
-    	expect(FactoryBot.build(:user, password: "qwer1234")).to_not be_valid
+      expect(FactoryBot.build(:user, password: "qwer1234")).to_not be_valid
     end
 
     it "must have at least one lower character" do
-    	expect(FactoryBot.build(:user, password: "QWER1234")).to_not be_valid
+      expect(FactoryBot.build(:user, password: "QWER1234")).to_not be_valid
     end
 
     it "must have at least one number" do
-    	expect(FactoryBot.build(:user, password: "Qwertyui")).to_not be_valid
+      expect(FactoryBot.build(:user, password: "Qwertyui")).to_not be_valid
     end
 
     it "must have at least 8 digits" do
-    	expect(FactoryBot.build(:user, password: "Qwer123")).to_not be_valid
+      expect(FactoryBot.build(:user, password: "Qwer123")).to_not be_valid
     end
   end
 end
