cc7d01304ebfdf14d5735018c9396ec692fec370
Sunil Pai <spai@robinsonsolutions.com>
2017-12-22 14:19:42 -0500

    Hotfix: cleaned up formatting of model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,26 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  enum role: { super_admin: "super_admin", admin: "admin", supervisor: "supervisor", planner: "planner", method_engineer: "method_engineer", quality: "quality", station: "station" }
+  enum role: {
+    super_admin: "super_admin",
+    admin: "admin",
+    supervisor: "supervisor",
+    planner: "planner",
+    method_engineer: "method_engineer",
+    quality: "quality",
+    station: "station"
+  }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   belongs_to :site
   has_many :accesses
+
   validates :email, presence: true
   validates :username, presence: true
-  validates :password, presence: true,
-  length: { :minimum => 8 },
-  format: { with: /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}/ }
+  validates :password,
+    presence: true,
+    length: { :minimum => 8 },
+    format: { with: /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}/ }
 end
