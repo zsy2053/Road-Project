266655d2678e367cd5bd4bddac5c91dec8056ccb
Sunil Pai <spai@robinsonsolutions.com>
2017-12-22 14:24:44 -0500

    Hotfix: removed references to removed helper class

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,8 +1,6 @@
 require 'devise'
-require Rails.root.join("spec/support/controller_macros.rb")
 
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, :type => :controller
   config.include Devise::Test::ControllerHelpers, :type => :view
-  config.include ControllerMacros, :type => :controller
 end
