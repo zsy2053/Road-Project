6075a93509672ea3e9749dac3c7b1cf993c6aca7
Sunil Pai <spai@robinsonsolutions.com>
2018-01-08 21:02:17 +0000

    Hotfix to migration file to properly add reference to users table for author

--- db/migrate/20180104211616_add_references_to_road_order.rb
+++ db/migrate/20180104211616_add_references_to_road_order.rb
@@ -1,6 +1,7 @@
 class AddReferencesToRoadOrder < ActiveRecord::Migration[5.1]
   def change
-  	add_reference :road_orders, :station, foreign_key: true
-  	add_reference :road_orders, :author, references: :users, foreign_key: true
+    add_reference :road_orders, :station, foreign_key: true
+    add_reference :road_orders, :author, references: :users, index: true
+    add_foreign_key :road_orders, :users, column: :author_id
   end
 end
