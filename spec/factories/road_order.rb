04c9bbfb1243d5d085e358281d1bfdb84603aff6
Henry Zhao <hzhao@robinsonsolutions.com>
2018-01-10 00:32:08 +0000

    ERO-152b: multiple contract accesses sql query error

--- spec/factories/road_order.rb
+++ spec/factories/road_order.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :road_order do
   	station
+  	contract
   	association :author, factory: :user
   	car_type "B"
   	start_car 1
--- spec/models/road_order_spec.rb
+++ spec/models/road_order_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe RoadOrder, type: :model do
   it { should belong_to(:station) }
-  it { should have_one(:contract) }
+  it { should belong_to(:contract) }
   it { should belong_to(:author) }
   it { should validate_presence_of :car_type }
   it { should validate_presence_of :start_car }
