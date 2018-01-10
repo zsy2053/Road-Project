0a87bb93ceaff4a71c83891d7c365bac50891c83
Sunil Pai <spai@robinsonsolutions.com>
2018-01-10 15:01:14 +0000

    Hotfix: default factory created a contract for the road order and a contract for the station resulting in each road order creating two contracts.

--- spec/factories/road_order.rb
+++ spec/factories/road_order.rb
@@ -1,9 +1,12 @@
 FactoryBot.define do
   factory :road_order do
-  	station
-  	contract
-  	association :author, factory: :user
-  	car_type "B"
-  	start_car 1
-	end
+    station
+    association :author, factory: :user
+    car_type "B"
+    start_car 1
+    
+    after(:build) do |road_order|
+      road_order.contract ||= road_order.station.contract
+    end
+  end
 end
