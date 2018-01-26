4ad7c7b3dbe1e4735dedbf6b55826a1b52d69aac
Sunil Pai <spai@robinsonsolutions.com>
2018-01-26 09:28:39 -0500

    Changed ability definitions to not use a loop on user Access objects

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,19 +13,20 @@ class Ability
         can :read, [RoadOrder]
       else
         can :read, Site, :id => user.site_id
-
-        user.accesses.each do |access|
-          # non super admins can only read contracts, stations, and road orders they have access to
-          can :read, Contract, :id => access.contract_id
-          can :read, Station, :contract_id => access.contract_id
-          can :read, RoadOrder, :contract_id => access.contract_id
-          can :read, BackOrder, :contract_id => access.contract_id
-          
-          # method engineers can also create road orders for their contracts
-          can :create, RoadOrder, :contract_id => access.contract_id if user.method_engineer?          
-          # planners can also create back orders for their contracts
-          can :create, BackOrder, :contract_id => access.contract_id if user.planner?
-        end
+        
+        user_contracts = user.contracts.pluck(:id)
+        
+        # non super admins can only read contracts, stations, and road orders they have access to
+        can :read, Contract, :id => user_contracts
+        can :read, Station, :contract_id => user_contracts
+        can :read, RoadOrder, :contract_id => user_contracts
+        can :read, BackOrder, :contract_id => user_contracts
+        
+        # method engineers can also create road orders for their contracts
+        can :create, RoadOrder, :contract_id => user_contracts if user.method_engineer?
+         
+        # planners can also create back orders for their contracts
+        can :create, BackOrder, :contract_id => user_contracts if user.planner?
 
         if user.admin?
           admin_accessible_roles = [ "admin", "supervisor", "planner", "method_engineer", "quality", "station" ]
