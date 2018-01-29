340bd9c428ee350ec69c7d5783d314721dd08b1a
Sunil Pai <spai@robinsonsolutions.com>
2018-01-29 14:59:05 -0500

    modified road order serializer to provide the station and contract as json and the elevated name attribute as "station_name" and "contract_name" like done in back order

--- app/serializers/road_order_serializer.rb
+++ app/serializers/road_order_serializer.rb
@@ -1,13 +1,13 @@
 class RoadOrderSerializer < ActiveModel::Serializer
-  attributes :id, :station, :contract, :car_type, :start_car, :work_centre, :module
+  attributes :id, :station, :station_name, :contract, :contract_name, :car_type, :start_car, :work_centre, :module
 
-  def station
+  def station_name
     if object.station
       object.station.name
     end
   end
 
-  def contract
+  def contract_name
     if object.contract
       object.contract.name
     end
