4b531368bf94a9b35a3679516ce85b408164671b
Naeimeh <nshakoori@robinsonsolutions.com>
2018-01-31 19:43:25 +0000

    ERO-257 Stations must be unique

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,6 @@
 class Station < ApplicationRecord
   belongs_to :contract
+  
+  validates :name, uniqueness: { scope: :contract }
+  validates :code, uniqueness: { scope: :contract }
 end
--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -6,4 +6,33 @@ RSpec.describe Station, type: :model do
   it "has a valid factory" do
     expect(FactoryBot.build(:station)).to be_valid
   end
+  
+  describe "name attributes" do
+    let!(:contract) { FactoryBot.create(:contract) }
+    let!(:station1) { FactoryBot.create(:station, name: "Station 1", contract: contract) }
+    
+    it "should validate uniqueness of station's name within a contract" do
+      station2 = FactoryBot.build(:station, name: "Station 1", contract: contract)
+      expect { station2.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+    
+    it "should not validate uniqueness of station's name across contracts" do
+      expect(FactoryBot.build(:station, name: "Station 1")).to be_valid
+    end
+  end
+  
+  describe "code attributes" do
+    let!(:contract) { FactoryBot.create(:contract) }
+    let!(:station1) { FactoryBot.create(:station, code: "S1", contract: contract) }
+    
+    it "should validate uniqueness of station's code within a contract" do
+      station2 = FactoryBot.build(:station, code: "S1", contract: contract)
+      expect { station2.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+    
+    it "should not validate uniqueness of station's code across contracts" do
+      expect(FactoryBot.build(:station, code: "S1")).to be_valid
+    end
+  end  
+
 end
