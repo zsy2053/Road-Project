c29792225747ed0f2d4d011c20c8896b68dea13e
Shuyang Zang <shuyang.zang@robinsonsolutions.com>
2018-02-19 18:37:01 +0000

    Hot-fix: Error when creating car road order

--- app/models/back_order.rb
+++ app/models/back_order.rb
@@ -1,10 +1,16 @@
 class BackOrder < ApplicationRecord
+  after_initialize :default_values
   before_save :set_contract
-  
+
   belongs_to :station
   belongs_to :contract
-  
+
   validates :cri, :focused_part_flag, :inclusion => {:in => [true, false]}
+
+  def default_values
+      self.cri = false if self.cri.nil?
+      self.focused_part_flag = false if self.focused_part_flag.nil?
+  end
   
   def set_contract
       self.contract = self.station.contract unless self.contract == self.station.contract
