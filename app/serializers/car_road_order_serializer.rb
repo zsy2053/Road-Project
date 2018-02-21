21701259878c328870c931bb0ad45c1db968d907
Shuyang Zang <shuyang.zang@robinsonsolutions.com>
2018-02-21 15:32:58 +0000

    ERO-273: Implement user flow in React

--- app/serializers/car_road_order_serializer.rb
+++ app/serializers/car_road_order_serializer.rb
@@ -1,63 +1,63 @@
 class CarRoadOrderSerializer < ActiveModel::Serializer
   attributes :id
-  
+
   belongs_to :car
   belongs_to :road_order
-  
+
   has_one :station, through: :road_order
   has_one :contract, through: :road_order
-  
+
   has_many :positions
   class PositionSerializer < ActiveModel::Serializer
-    attributes :id, :name, :allows_multiple
+    attributes :id, :name, :allows_multiple, :operators
   end
-  
+
   has_many :movements
   class MovementSerializer < ActiveModel::Serializer
     attributes :id, :work_location, :actual_duration, :comments, :percent_complete,
       :day, :shift, :name, :description, :expected_duration, :breaks, :expected_start,
       :expected_end, :serialized, :positions, :production_critical, :quality_critical
-    
+
     def work_location
       object.definition.work_location
     end
-    
+
     def day
       object.definition.day
     end
-    
+
     def shift
       object.definition.shift
     end
-    
+
     def name
       object.definition.name
     end
-    
+
     def description
       object.definition.description
     end
-    
+
     def expected_duration
       object.definition.expected_duration
     end
-    
+
     def breaks
       object.definition.breaks
     end
-    
+
     def expected_start
       object.definition.expected_start
     end
-    
+
     def expected_end
       object.definition.expected_end
     end
-    
+
     def serialized
       object.definition.serialized
     end
-    
+
     def positions
       object.definition.positions
     end
--- spec/serializers/car_road_order_serializer_spec.rb
+++ spec/serializers/car_road_order_serializer_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe CarRoadOrderSerializer, type: :serializer do
   let(:serialization) { ActiveModelSerializers::Adapter.create(serializer) }
 
   let(:subject) { JSON.parse(serialization.to_json) }
-    
+
   it 'only contains expected keys' do
     expect(subject.keys).to contain_exactly(
       'id',
@@ -17,5 +17,46 @@ RSpec.describe CarRoadOrderSerializer, type: :serializer do
       'positions',
       'movements'
     )
+  end
+
+  context "position attributes" do
+    let(:operator2) { FactoryBot.create(:operator)}
+    let(:position2) { FactoryBot.create(:position, operators: [operator2]) }
+    let(:car_road_order2) { FactoryBot.create(:car_road_order, positions: [position2] ) }
+    let(:serializer2) { described_class.new(car_road_order2) }
+    let(:serialization2) { ActiveModelSerializers::Adapter.create(serializer2) }
+
+    let(:subject2) { JSON.parse(serialization2.to_json) }
+    it 'only contains expect keys' do
+      expect(subject2['positions'][0].keys).to contain_exactly(
+      'id',
+      'name',
+      'allows_multiple',
+      'operators'
+      )
+    end
+
+    it 'verify operators' do
+      expect(subject2['positions'][0]['operators'][0].keys).to contain_exactly(
+      'first_name',
+      'last_name',
+      'employee_number',
+      'badge',
+      'suspended',
+      'created_at',
+      'updated_at',
+      'id',
+      'position_id',
+      'site_id'
+      )
+      expect(subject2['positions'][0]['operators'][0]['first_name']).to eq(operator2.first_name)
+      expect(subject2['positions'][0]['operators'][0]['last_name']).to eq(operator2.last_name)
+      expect(subject2['positions'][0]['operators'][0]['employee_number']).to eq(operator2.employee_number)
+      expect(subject2['positions'][0]['operators'][0]['badge']).to eq(operator2.badge)
+      expect(subject2['positions'][0]['operators'][0]['suspended']).to eq(operator2.suspended)
+      expect(subject2['positions'][0]['operators'][0]['id']).to eq(operator2.id)
+      expect(subject2['positions'][0]['operators'][0]['position_id']).to eq(position2.id)
+      expect(subject2['positions'][0]['operators'][0]['site_id']).to eq(operator2.site_id)
+    end
   end
 end
