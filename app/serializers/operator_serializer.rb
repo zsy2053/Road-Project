1fcf4ef9e795a5f064596e38da023fc4cdb07028
Shuyang Zang <shuyang.zang@robinsonsolutions.com>
2018-02-27 00:34:42 +0000

    ERO-357: Add serializer to include operator's position

--- app/serializers/operator_serializer.rb
+++ app/serializers/operator_serializer.rb
@@ -1,3 +1,3 @@
 class OperatorSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :employee_number, :suspended
+  attributes :id, :first_name, :last_name, :employee_number, :suspended, :position_id
 end
