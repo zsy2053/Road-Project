89d15bdf521a03be63c36e7851a762997f088a28
Sunil Pai <spai@robinsonsolutions.com>
2018-03-06 10:45:12 -0500

    Updated specs to ensure updated_at attribute for operators removed from positions get updated and modified controller to do so.

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -12,7 +12,16 @@ class PositionsController < ApplicationController
   def update
     @position = Position.find(params[:id])
     authorize! :update, @position
+    
+    # keep track of previous operator ids
+    old_ids = @position.operators.pluck(:id)
+    
+    # attempt to update position to update foreign keys on operators
     if @position.update(operators: Operator.where(id: params[:operator_ids]))
+      # update the updated_at attributes for the operators removed from the position since Rails won't
+      removed_ids = if params[:operator_ids].nil? then old_ids else (old_ids - params[:operator_ids]) end
+      Operator.where(id: removed_ids).map(&:touch)
+      
       render json: @position
     else
       render json: @position.errors, status: :unprocessable_entity
