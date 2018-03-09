8df236a5e8fd335004a622bcc99050c1e58bf8c4
hzhao <hzhao@robinsonsolutions.com>
2018-03-09 11:50:38 -0500

    Add the error message for last name and first name.

--- app/models/operator.rb
+++ app/models/operator.rb
@@ -3,5 +3,7 @@ class Operator < ApplicationRecord
   belongs_to :position, optional: true
   belongs_to :movement, optional: true
   validates_uniqueness_of :employee_number, :badge
-  validates_presence_of :employee_number, :badge
+  validates_presence_of :employee_number, :badge, :last_name, :first_name
+  validates_length_of :last_name, :minimum => 2
+  validates_length_of :first_name, :minimum => 2
 end
